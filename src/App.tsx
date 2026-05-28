65f25b0be5b2a7fbcfd8f6aa7229c0322eb00e3c
luizfernandogl12345@gmail.com <luizfernandogl12345@gmail.com>
2026-05-28 15:17:56 -0300

    Ultima parte de aprendizado estrutural

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import './styles/theme.css'
 import './styles/global.css'
+import { Heading } from './components/Heading'
+import { TimerIcon } from 'lucide-react';
 
 export function App(){
 
     return(
     <>
-        <h1>Ola mundo(do App)</h1>
+        <Heading>
+            Iai meus fi
+            <button>
+                <TimerIcon/>
+            </button>
+        </Heading>
+
         <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore aperiam ipsam suscipit, illo, sapiente, adipisci laborum distinctio veritatis repudiandae minima doloremque porro dolorem! Maxime maiores pariatur eveniet, porro excepturi aliquid.
         </p>
