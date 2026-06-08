a750b292b17f93577a95bcc4f410141b9ee20cc9
luizfernandogl12345@gmail.com <luizfernandogl12345@gmail.com>
2026-06-08 19:49:00 -0300

    aprendendo sobre o useState e sobre efeito colateral

--- src/components/Menu/Index.tsx
+++ src/components/Menu/Index.tsx
@@ -1,23 +1,47 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
 import styles from './styles.module.css';
+import { useState } from 'react';
+
+type AvailableThemes = 'dark' | 'light'
 
 export function Menu(){
-    return(
-        <nav className={styles.menu}>
 
-            <a className={styles.icon} href="#">
+    const [themes, setThemes] = useState<AvailableThemes>('dark');
+
+    function handleThemeChangeClick(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>){
+        console.log('Clicado', Date.now());
+
+        event.preventDefault();
+
+        setThemes(prevTheme => {
+            const nextTheme = prevTheme === 'dark' ? 'light': 'dark';
+            return nextTheme;
+        })
+
+        document.documentElement.setAttribute('data-theme', themes)
+    }
+
+
+
+    return(
+        
+        <nav className={styles.menu}>
+            <h1>{themes}</h1>
+
+
+            <a className={styles.icon} href="#" aria-label='Ir para Home' title='Ir para Home'>
                 <HouseIcon/>
             </a>
 
-            <a className={styles.icon} href="#">
+            <a className={styles.icon} href="#"  aria-label='Ir para Historico' title='Ir para Historico'>
                 <HistoryIcon/>
             </a>
 
-            <a className={styles.icon} href="#">
+            <a className={styles.icon} href="#"  aria-label='Ir para Configuracoes' title='Ir para Configuracoes'>
                 <SettingsIcon/>
             </a>
 
-            <a className={styles.icon} href="#">
+            <a className={styles.icon} href="#" aria-label='Trocar tema' title='Trocar tema' onClick={handleThemeChangeClick}>
                 <SunIcon/>
             </a>
 
